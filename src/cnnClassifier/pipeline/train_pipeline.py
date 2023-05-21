0419f13d7108e681091119ccb485ac752040ea49
santhoshmlops <santhoshmlops@gmail.com>
2023-05-21 12:49:19 +0530

    preared call back

--- src/cnnClassifier/pipeline/train_pipeline.py
+++ src/cnnClassifier/pipeline/train_pipeline.py
@@ -2,6 +2,7 @@
 from cnnClassifier.configuration.config import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier.components.prepare_base_model import PrepareBaseModel
+from cnnClassifier.components.prepare_callback import PrepareCallback
 
 
 class DataIngestionTrainingPipeline:
@@ -26,3 +27,23 @@ class PrepareBaseModelTrainingPipeline:
         prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
         prepare_base_model.get_base_model()
         prepare_base_model.update_base_model()
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_callbacks_config = config.get_prepare_callback_config()
+        prepare_callbacks = PrepareCallback(config=prepare_callbacks_config)
+        callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
+
+
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train(
+            callback_list=callback_list
+        )
+        
